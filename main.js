1395c345039da8094f93f30cf00b8d69fcc96dde
aaronmccollum <mccollumaarons@gmail.com>
2026-01-29 17:13:47 +0900

    feat: added boilerplate code for JS and HTML and Tailwind

---
+++ main.js
@@ -0,0 +1,5 @@
+import { BudgetItem } from "./classes/BudgetItem"
+import { FixedExpense } from "./classes/FixedExpense"
+import { UI } from "./utils/UI"
+
+// Code to put it all together
---
+++ utils/UI.js
@@ -0,0 +1,6 @@
+export class Display {
+    static showBudgetItem(item) {
+        const selectedBudgetItem = document.querySelector("select").value;
+        const amount = document.querySelector("input").value;
+    }
+}
